89f3838ffc1dbcb97abf8148d8ac3c7df39820db
Giovanni Abbiendi <sha1-03ca615cb6d62d8479399a2c21824cf89dd4fac5@cern.ch>
2008-10-02 10:59:56 +0000

    python example analyzer for MuonAssociatorByHits

---
+++ test/testMyAssociator_cfg.py
@@ -0,0 +1,160 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("myana")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:RelVal.root')
+)
+
+# MessageLogger
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.MessageLogger.categories = cms.untracked.vstring('testMyAssociator',
+    'MuonAssociatorByHits', 'DTHitAssociator', 'RPCHitAssociator', 'MuonTruth', 'PSimHitMap',
+    'FwkJob', 'FwkReport', 'FwkSummary', 'Root_NoDictionary')
+
+process.MessageLogger.cout = cms.untracked.PSet(
+    noTimeStamps = cms.untracked.bool(True),
+    threshold = cms.untracked.string('INFO'),
+    INFO = cms.untracked.PSet(
+        limit = cms.untracked.int32(0)
+    ),
+    default = cms.untracked.PSet(
+        limit = cms.untracked.int32(10000000)
+    ),
+    testMyAssociator = cms.untracked.PSet(
+        limit = cms.untracked.int32(10000000)
+    ),
+    MuonAssociatorByHits = cms.untracked.PSet(
+        limit = cms.untracked.int32(0)
+    ),
+    DTHitAssociator = cms.untracked.PSet(
+        limit = cms.untracked.int32(0)
+    ),
+    RPCHitAssociator = cms.untracked.PSet(
+        limit = cms.untracked.int32(0)
+    ),
+    MuonTruth = cms.untracked.PSet(
+        limit = cms.untracked.int32(0)
+    ),
+    PSimHitMap = cms.untracked.PSet(
+        limit = cms.untracked.int32(0)
+    ),
+    FwkReport = cms.untracked.PSet(
+        reportEvery = cms.untracked.int32(1),
+        limit = cms.untracked.int32(10000000)
+    ),
+    FwkSummary = cms.untracked.PSet(
+        reportEvery = cms.untracked.int32(1),
+        limit = cms.untracked.int32(10000000)
+    ),
+    FwkJob = cms.untracked.PSet(
+        limit = cms.untracked.int32(0)
+    ),
+    Root_NoDictionary = cms.untracked.PSet(
+        limit = cms.untracked.int32(0)
+    )
+)
+
+process.MessageLogger.statistics = cms.untracked.vstring('cout')
+
+process.MessageLogger.cerr = cms.untracked.PSet(
+    placeholder = cms.untracked.bool(True)
+)
+
+#process.Tracer = cms.Service("Tracer")
+
+# Mixing Module
+process.load("SimGeneral.MixingModule.mixNoPU_cfi")
+
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.FakeConditions_cff")
+
+# --- Analyzer running MuonAssociatorByHits 
+process.testanalyzer = cms.EDAnalyzer("testMyAssociator",
+    # for Muon Track association
+    #
+    #     input collections
+    #
+    # ... reco::Track collection
+    tracksTag = cms.InputTag("standAloneMuons"),
+    # tracksTag = cms.InputTag("standAloneMuons","UpdatedAtVtx"),
+    # tracksTag = cms.InputTag("globalMuons"),
+    # tracksTag = cms.InputTag("generalTracks"),
+    #
+    # ... TrackingParticle collection
+    tpTag = cms.InputTag("mergedtruth","MergedTrackTruth"),
+    #
+    dumpInputCollections = cms.bool(False),
+    #
+    #....... general input parameters
+    #
+    AbsoluteNumberOfHits_track = cms.bool(False),
+    MinHitCut_track = cms.uint32(1),
+    AbsoluteNumberOfHits_muon = cms.bool(False),
+    MinHitCut_muon = cms.uint32(1),
+    #
+    PurityCut_track = cms.double(0.5),
+    PurityCut_muon = cms.double(0.5),
+    #
+    SimToReco_useTracker = cms.bool(False),
+    EfficiencyCut_track = cms.double(0.5),
+    #
+    SimToReco_useMuon = cms.bool(True),
+    EfficiencyCut_muon = cms.double(0.5),
+    #
+    #........(for inner tracker stub of Global Muons)...
+    UsePixels = cms.bool(True),
+    UseGrouped = cms.bool(True),
+    UseSplitting = cms.bool(True),
+    ThreeHitTracksAreSpecial = cms.bool(True),
+    #
+    # for DT Hit associator
+    crossingframe = cms.bool(True),
+    simtracksTag = cms.InputTag("g4SimHits"),
+    simtracksXFTag = cms.InputTag("mix","g4SimHits"),
+    #
+    DTsimhitsTag = cms.InputTag("g4SimHits","MuonDTHits"),
+    DTsimhitsXFTag = cms.InputTag("mix","g4SimHitsMuonDTHits"),
+    DTdigiTag = cms.InputTag("simMuonDTDigis"),
+    DTdigisimlinkTag = cms.InputTag("simMuonDTDigis"),
+    DTrechitTag = cms.InputTag("dt1DRecHits"),
+    #
+    dumpDT = cms.bool(False),
+    links_exist = cms.bool(True),
+    associatorByWire = cms.bool(False),
+    #
+    # for CSC Hit associator
+    CSCsimHitsXFTag = cms.InputTag("mix","g4SimHitsMuonCSCHits"),
+    CSClinksTag = cms.InputTag("simMuonCSCDigis","MuonCSCStripDigiSimLinks"),
+    CSCwireLinksTag = cms.InputTag("simMuonCSCDigis","MuonCSCWireDigiSimLinks"),
+    #
+    # for RPC Hit associator
+    RPCsimhitsXFTag = cms.InputTag("mix","g4SimHitsMuonRPCHits"),
+    RPCdigisimlinkTag = cms.InputTag("simMuonRPCDigis","RPCDigiSimLink"),
+    #
+    # for Tracker Hit associator
+    #
+    associatePixel = cms.bool(True),
+    associateStrip = cms.bool(True),
+    associateRecoTracks = cms.bool(True),
+    #                                
+    ROUList = cms.vstring('TrackerHitsTIBLowTof', 
+        'TrackerHitsTIBHighTof', 
+        'TrackerHitsTIDLowTof', 
+        'TrackerHitsTIDHighTof', 
+        'TrackerHitsTOBLowTof', 
+        'TrackerHitsTOBHighTof', 
+        'TrackerHitsTECLowTof', 
+        'TrackerHitsTECHighTof', 
+        'TrackerHitsPixelBarrelLowTof', 
+        'TrackerHitsPixelBarrelHighTof', 
+        'TrackerHitsPixelEndcapLowTof', 
+        'TrackerHitsPixelEndcapHighTof')
+)
+
+process.test = cms.Path(process.mix * process.testanalyzer)
